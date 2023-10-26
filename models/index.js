1647cb0eeea7ec044ee5c4172556621772563de0
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-10-26 22:04:16 +0300

    feat(models): add brand model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 module.exports.User = require("./User.js");
 module.exports.Category = require("./Category.js");
 module.exports.Subcategory = require("./Subcategory.js");
+module.exports.Brand = require("./Brand.js");
