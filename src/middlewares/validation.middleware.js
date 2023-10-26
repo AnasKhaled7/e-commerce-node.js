10036aa5da23e6e953d2e951b7a65f5337b9efa2
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-10-26 23:55:55 +0300

    feat(middlewares): add validation middleware

---
+++ src/middlewares/validation.middleware.js
@@ -0,0 +1,22 @@
+const isValid = (schema) => {
+  return (req, res, next) => {
+    try {
+      const requests = { ...req.body, ...req.params, ...req.query };
+
+      const validationResult = schema.validate(requests, { abortEarly: false });
+
+      if (validationResult.error) {
+        const messages = validationResult.error.details.map(
+          (error) => error.message
+        );
+        return res.status(400).json({ success: false, messages });
+      }
+
+      return next();
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error.message });
+    }
+  };
+};
+
+module.exports = isValid;
