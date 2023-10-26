d6b17efc574a08a584870155d56d9fed4ef43281
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-10-26 23:44:42 +0200

    feat(utils): add send email function

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,19 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // send mail with defined transport object
+  const emailInfo = await transporter.sendMail(options);
+
+  return emailInfo.accepted.length < 1 ? false : true;
+};
+
+module.exports = sendEmail;
