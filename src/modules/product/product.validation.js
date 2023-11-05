360522def7065dbeb07d58f2bef4258ed1af543b
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-11-05 19:04:39 +0200

    fix(products): fix validation issue

--- src/modules/product/product.validation.js
+++ src/modules/product/product.validation.js
@@ -8,24 +8,9 @@ export const createProductSchema = joi
     name: joi.string().trim().required(),
     description: joi.string().trim().required(),
     price: joi.number().min(1).required(),
-    quantity: joi.number().min(1).required(),
     category: joi.string().custom(isValidObjectId).required(),
     brand: joi.string().custom(isValidObjectId).required(),
-    defaultImage: joi
-      .object({
-        id: joi.string().required(),
-        url: joi.string().required(),
-      })
-      .required(),
-    images: joi
-      .array()
-      .items(
-        joi.object({
-          id: joi.string().required(),
-          url: joi.string().required(),
-        })
-      )
-      .required(),
+    countInStock: joi.number().min(1).required(),
     discount: joi.number().min(0).max(100).default(0),
   })
   .required();
@@ -71,19 +56,9 @@ export const updateProductSchema = joi
     name: joi.string().trim(),
     description: joi.string().trim(),
     price: joi.number().min(1),
-    quantity: joi.number().min(1),
+    countInStock: joi.number().min(1),
     category: joi.string().custom(isValidObjectId),
     brand: joi.string().custom(isValidObjectId),
-    defaultImage: joi.object({
-      id: joi.string().required(),
-      url: joi.string().required(),
-    }),
-    images: joi.array().items(
-      joi.object({
-        id: joi.string().required(),
-        url: joi.string().required(),
-      })
-    ),
     discount: joi.number().min(0).max(100).default(0),
   })
   .required();
