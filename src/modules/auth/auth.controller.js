3ca6449ce6070e73f845baf05bbce6c5fb98f59c
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-11-12 23:55:00 +0200

    refactor(auth): use new token generation method

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,84 +1,75 @@
-import bcryptjs from "bcryptjs";
-import cryptoJS from "crypto-js";
-import jwt from "jsonwebtoken";
 import crypto from "crypto";
 
-import { User, Cart, Token } from "../../models/index.js";
+import { User, Cart } from "../../models/index.js";
 import asyncHandler from "../../utils/asyncHandler.js";
+import generateToken from "../../utils/generateToken.js";
 import sendEmail from "../../utils/sendEmail.js";
 
-// register
+// @desc     Register
+// @route    POST /api/v1/auth/register
+// @access   Public
 export const register = asyncHandler(async (req, res, next) => {
   const { firstName, lastName, email, password, phone, gender } = req.body;
 
   // check user existence
-  const isUser = await User.findOne({ email });
-  if (isUser) return next(new Error("User already exists", { cause: 409 }));
-
-  // hash password
-  const hashPassword = bcryptjs.hashSync(
-    password,
-    Number(process.env.SALT_ROUND)
-  );
-
-  // encrypt phone number
-  const encryptPhone = cryptoJS.AES.encrypt(
-    phone,
-    process.env.ENCRYPTION_KEY
-  ).toString();
+  const isUserExist = await User.findOne({ email });
+  if (isUserExist)
+    return next(new Error("User already exists", { cause: 409 }));
 
   // create user
   const user = await User.create({
     firstName,
     lastName,
     email,
-    password: hashPassword,
-    phone: encryptPhone,
+    password,
+    phone,
     gender,
   });
 
   // create cart
   await Cart.create({ user: user._id });
 
-  return res.status(201).json({ success: true, message: "User created" });
+  // generate token
+  generateToken(res, user._id);
+
+  return res.status(201).json({
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    role: user.role,
+  });
 });
 
-// login
+// @desc     Login
+// @route    POST /api/v1/auth/login
+// @access   Public
 export const login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
 
   // check user existence
   const user = await User.findOne({ email });
-  if (!user) return next(new Error("User not found", { cause: 404 }));
+  if (!user) return next(new Error("Invalid credentials", { cause: 400 }));
 
   // check password
-  const isMatch = bcryptjs.compareSync(password, user.password);
+  const isMatch = await user.matchPassword(password);
   if (!isMatch) return next(new Error("Invalid credentials", { cause: 400 }));
 
   // generate token
-  const token = jwt.sign(
-    {
-      _id: user._id,
-      email: user.email,
-      role: user.role,
-      isBlocked: user.isBlocked.status,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRE }
-  );
+  generateToken(res, user._id);
 
-  // save token
-  await Token.create({
-    token,
-    user: user._id,
-    agent: req.headers["user-agent"],
-    expireAt: new Date(jwt.decode(token).exp * 1000),
+  return res.status(200).json({
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    role: user.role,
   });
-
-  return res.status(200).json({ success: true, token });
 });
 
-// send reset password code to email
+// @desc     Send reset password code to email
+// @route    PATCH /api/v1/auth/reset-password-code
+// @access   Public
 export const sendResetPasswordCode = asyncHandler(async (req, res, next) => {
   const { email } = req.body;
 
@@ -104,7 +95,9 @@ export const sendResetPasswordCode = asyncHandler(async (req, res, next) => {
   return res.status(200).json({ success: true, message: "Email sent" });
 });
 
-// reset password
+// @desc     Reset password
+// @route    PATCH /api/v1/auth/reset-password
+// @access   Public
 export const resetPassword = asyncHandler(async (req, res, next) => {
   const { email, code, password } = req.body;
 
@@ -116,19 +109,10 @@ export const resetPassword = asyncHandler(async (req, res, next) => {
   if (user.resetPasswordCode !== code)
     return next(new Error("Invalid code", { cause: 400 }));
 
-  // hash password
-  const hashPassword = bcryptjs.hashSync(
-    password,
-    Number(process.env.SALT_ROUND)
-  );
-
   // update password
-  user.password = hashPassword;
+  user.password = password;
   user.resetPasswordCode = undefined;
   await user.save();
-
-  // delete tokens
-  await Token.deleteMany({ user: user._id });
 
   return res.status(200).json({ success: true, message: "Password reset" });
 });
