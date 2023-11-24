52dcc7adb5d797a569537d6cbc5ece80cb3ff6b9
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-11-24 23:26:03 +0200

    refactor(category): to include route descriptions

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -2,15 +2,15 @@ import { Category } from "../../models/index.js";
 import asyncHandler from "../../utils/asyncHandler.js";
 import cloudinary from "../../utils/cloud.js";
 
-// create category
+// @desc      Create category
+// @route     POST /api/v1/categories
+// @access    Private/Admin
 export const createCategory = asyncHandler(async (req, res, next) => {
-  const { name } = req.body;
-
   // file
   if (!req.file) return next(new Error("Image is required", { cause: 400 }));
 
   // check if the category is already exists
-  const isCategoryExist = await Category.findOne({ name });
+  const isCategoryExist = await Category.findOne({ name: req.body.name });
   if (isCategoryExist)
     return next(new Error("Category already exists", { cause: 409 }));
 
@@ -22,7 +22,7 @@ export const createCategory = asyncHandler(async (req, res, next) => {
 
   // create category
   const category = await Category.create({
-    name,
+    name: req.body.name,
     image: { url: secure_url, id: public_id },
     user: req.user._id,
   });
@@ -34,7 +34,9 @@ export const createCategory = asyncHandler(async (req, res, next) => {
   });
 });
 
-// get all categories
+// @desc      Get all categories
+// @route     GET /api/v1/categories
+// @access    Public
 export const getCategories = asyncHandler(async (req, res) => {
   let { page, limit, search } = req.query;
 
@@ -52,7 +54,7 @@ export const getCategories = asyncHandler(async (req, res) => {
     { $sort: { createdAt: -1 } },
     { $skip: (page - 1) * limit },
     { $limit: Number(limit) },
-    { $project: { name: 1, image: 1, createdAt: 1 } },
+    { $project: { name: 1, image: 1 } },
   ]);
 
   return res.status(200).json({
@@ -64,9 +66,13 @@ export const getCategories = asyncHandler(async (req, res) => {
   });
 });
 
-// get category by id
+// @desc      Get category by id
+// @route     GET /api/v1/categories/:categoryId
+// @access    Public
 export const getCategory = asyncHandler(async (req, res, next) => {
-  const category = await Category.findById(req.params.categoryId);
+  const category = await Category.findById(req.params.categoryId).select(
+    "name"
+  );
 
   // check if the category is found
   if (!category) return next(new Error("Category not found", { cause: 404 }));
@@ -74,12 +80,14 @@ export const getCategory = asyncHandler(async (req, res, next) => {
   return res.status(200).json({ success: true, category });
 });
 
-// update category by id
+// @desc      Update category by id
+// @route     PUT /api/v1/categories/:categoryId
+// @access    Private/Admin
 export const updateCategory = asyncHandler(async (req, res, next) => {
   const { name } = req.body;
 
   // check if body is empty
-  if (!name && !req.file)
+  if (!name || !req.file)
     return next(new Error("Name or image is required", { cause: 400 }));
 
   // check if the category is found
@@ -110,7 +118,9 @@ export const updateCategory = asyncHandler(async (req, res, next) => {
     .json({ success: true, message: "Category updated successfully" });
 });
 
-// delete category by id
+// @desc      Delete category by id
+// @route     DELETE /api/v1/categories/:categoryId
+// @access    Private/Admin
 export const deleteCategory = asyncHandler(async (req, res, next) => {
   const category = await Category.findByIdAndDelete(req.params.categoryId);
 
